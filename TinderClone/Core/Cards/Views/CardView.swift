51ea5bc5cf82c6352f34a32b6269e6dfd2322226
Marco Alonso <marcoalonso@192.168.1.21>
2024-03-13 21:49:10 -0600

    Module 3 Swiping cards animation

--- TinderClone/Core/Cards/Views/CardView.swift
+++ TinderClone/Core/Cards/Views/CardView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct CardView: View {
+    @State private var xOffset: CGFloat = 0
+    @State private var degrees: Double = 0
+    
+    
     var body: some View {
         ZStack(alignment: .bottom) {
             Image("megan")
@@ -22,11 +26,40 @@ struct CardView: View {
                 .padding(.horizontal, 10)
                 
         }
+        .offset(x: xOffset)
+        .rotationEffect(.degrees(degrees))
+        .animation(.snappy, value: xOffset)
+        .gesture(
+        DragGesture()
+            .onChanged(onDragChanged)
+            .onEnded(onDragEnded)
+        )
         
     }
 }
 
 private extension CardView {
+    
+    func onDragChanged(_ value: _ChangedGesture<DragGesture>.Value) {
+        xOffset = value.translation.width
+        degrees = Double(value.translation.width / 25)
+    }
+    
+    func onDragEnded(_ value: _ChangedGesture<DragGesture>.Value) {
+        let width = value.translation.width
+        
+        if abs(width) <=  abs(screenCutoff) {
+            xOffset = 0
+            degrees = 0
+        }
+    }
+}
+
+private extension CardView {
+    var screenCutoff: CGFloat {
+        UIScreen.main.bounds.width / 2 * 0.95
+    }
+    
     var cardWidth: CGFloat {
        UIScreen.main.bounds.width - 20
    }
